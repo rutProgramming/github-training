bb5736c3f33217495d3092145896c0d265c56226
Ruth shtraicher <RutProgramming8978@gmail.com>
2025-10-21 13:24:11 +0300

    fix

--- app/books/page.jsx
+++ app/books/page.jsx
@@ -1,13 +1,15 @@
-'use client';
+"use client";
 
 export default function BooksPage() {
   return (
-    <header>
-      <h1>Books Page</h1>
-      <p>Welcome to the Books section of our application.</p>
-    </header>
-    <main>
-            <h1>Home Page</h1>
-        </main>
+    <>
+      <header>
+        <h1>Books Page</h1>
+        <p>Welcome to the Books section of our application.</p>
+      </header>
+      <main>
+        <h1>Home Page</h1>
+      </main>
+    </>
   );
 }
