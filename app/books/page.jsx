1e061bd89d2a0a6ec107f306787d2fb3f1786d84
shira-miller <sm0504123007@gmail.com>
2025-10-21 14:25:49 +0300

    refactor: format JSX for better readability in BooksPage component

--- app/books/page.jsx
+++ app/books/page.jsx
@@ -1,18 +1,18 @@
 "use client";
 
 export default function BooksPage() {
-  return (
-    <>
-    <header>
-      <h1>Books Page</h1>
-      <p>Welcome to the Books section of our application.</p>
-    </header>
-    <main>
-            <h1>Home Page</h1>
-        </main>
-        <footer>
-            footer
-        </footer>
-    </>
-  );
+    return (
+        <>
+            <header>
+                <h1>Books Page</h1>
+                <p>Welcome to the Books section of our application.</p>
+            </header>
+            <main>
+                <h1>Home Page</h1>
+            </main>
+            <footer>
+                footer
+            </footer>
+        </>
+    );
 }
