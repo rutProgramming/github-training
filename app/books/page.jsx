3a37966cf81b74be4858d2f33832ae718675e1fd
Ruth shtraicher <RutProgramming8978@gmail.com>
2025-10-21 13:16:17 +0300

    header

---
+++ app/books/page.jsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function BooksPage() {
+  return (
+    <header>
+      <h1>Books Page</h1>
+      <p>Welcome to the Books section of our application.</p>
+    </header>
+  );
+}
